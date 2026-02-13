911e1cdfa241a096c2b2e3103440c23447c45b73
Ryan Schmidt <ryan@skycatchfire.com>
2026-02-13 15:14:57 +0000

    adds clean-text script for raycast

---
+++ clean-text.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Clean Text
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+pbpaste | awk '
+  /^[[:space:]]*$/ {
+    if (buf) print buf
+    buf = ""
+    print ""
+    next
+  }
+  {
+    gsub(/^[[:space:]]+/, "")
+    if (/^- /) {
+      if (buf) print buf
+      buf = $0
+    } else {
+      buf = (buf ? buf " " $0 : $0)
+    }
+  }
+  END { if (buf) print buf }
+' | sed '/^$/{ N; /^\n$/d; }' | pbcopy
+
+osascript -e '
+  delay 0.5
+  tell application "System Events" to keystroke "v" using command down'
